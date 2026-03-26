f51e7aed6afe7a806e883376da50f47765c93689
panquemadito <todopanquemadito@gmail.com>
2026-03-26 19:01:43 -0300

    ejercicio2

---
+++ Ejercicio2/ejercicio2Paralela/src/main/java/com/mycompany/ejercicio2paralela/Ejercicio2Paralela.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2paralela;
+
+/**
+ *
+ * @author fguzm
+ */
+public class Ejercicio2Paralela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
